8e65143b1238ffdb072a9676104c8151cb661845
zht <993887459@qq.com>
2018-11-27 10:34:26 +0800

    添加项目文件。

---
+++ Edu.Swagger/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edu.Swagger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        /// <summary>
+        /// GET方法
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ApiExplorerSettings(GroupName = "v2")]
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        /// <summary>
+        /// POST方法
+        /// </summary>
+        /// <param name="value"></param>
+        [ApiExplorerSettings(GroupName = "v2")]
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <remarks>
+        /// 例子:
+        /// Get api/Values/1
+        /// </remarks>
+        /// <param name="id">主键</param>
+        /// <param name="value">测试字符串</param>
+        [ApiExplorerSettings(GroupName = "v1")]
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">主键</param>
+        [ApiExplorerSettings(GroupName = "v1")]
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ Edu.Swagger/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Edu.Swagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "第一版 API",
+                    Description = "第一版 ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "张海涛",
+                        Email = "993887459@qq.com",
+                        Url = "https://blog.csdn.net/haitaoDoit"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+
+                option.SwaggerDoc("v2", new Info
+                {
+                    Version = "v2",
+                    Title = "第二版 API",
+                    Description = "第二版 ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "张海涛",
+                        Email = "993887459@qq.com",
+                        Url = "https://blog.csdn.net/haitaoDoit"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+                
+                // 为 Swagger JSON and UI设置xml文档注释路径
+                option.IncludeXmlComments(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Edu.Swagger.xml"));
+            });
+            }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+                //c.RoutePrefix = "swagger";
+            });
+
+            app.UseMvc();
+        }
+    }
+}
